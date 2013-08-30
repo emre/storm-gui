5e0a663db8547343772342d43934abaa8c32d6af
Emre Yılmaz <mail@emreyilmaz.me>
2013-08-30 19:07:49 +0300

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='storm-gui',
-    version='0.1',
+    version='0.2',
     packages=['storm_gui'],
     url='http://github.com/emre/storm-gui',
     license='MIT',
