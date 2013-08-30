7ea8909aa5cc76f6e1019f81db21b011af9a19ba
Emre Yılmaz <mail@emreyilmaz.me>
2013-08-30 19:13:50 +0300

    fix description

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name='stormssh-gui',
-    version='0.3',
+    version='0.4',
     packages=['storm_gui'],
     url='http://github.com/emre/storm-gui',
     license='MIT',
     author='Emre Yilmaz',
     author_email='mail@emreyilmaz.me',
-    description='a gui application form stormssh',
+    description='a gui application for stormssh',
     install_requires=["stormssh", ],
     entry_points={
         'console_scripts': ['storm-gui = storm_gui.storm_gui:main',]
